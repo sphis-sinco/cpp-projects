2e0135cd6a255ff94626c80d9ff60bcb4dce4526
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-28 17:43:43 -0500

    good

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,5 +4,8 @@ int main()
 {
         std::cout << "Hello world!" << std::endl;
         std::cout << "How are you?" << std::endl;
+
+        std::cout << "I'm good. How about you?" << std::endl;
+
         return 0;
 }
