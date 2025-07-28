cbbee54c5cc37b5d916f0e70185cc77184bce041
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-28 18:01:30 -0500

    ages

---
+++ projects/03 - namespaces/namespaces.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+namespace bob{
+        int age = 30;
+}
+namespace sally{
+        int age = 25;
+}
+
+int main()
+{
+        std::cout << "What's Bob's age again?\n" << bob::age << " right?";
+        std::cout << "And sallies...\n" << sally::age << " right?";
+
+        return 0;
+}
