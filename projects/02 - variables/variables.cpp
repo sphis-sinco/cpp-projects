86747d27e79778a756fd9a7c43121ca4600ef670
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-28 17:59:16 -0500

    constants

--- projects/02 - variables/variables.cpp
+++ projects/02 - variables/variables.cpp
@@ -3,39 +3,58 @@
 int main()
 {
         // int (whole number no decimal)
-        int integer = 5;
+        const int INTEGER = 5;
         // double (number with decimal, a float)
-        double adouble = 3.14159;
+        const double ADOUBLE = 3.14159;
         // single character
-        char character = 'A';
+        const char CHARACTER = 'A';
         // true/false
+        const bool ORIGINAL_ALIVE_VALUE = true;
         bool alive = true;
         // phrase
-        std::string feeling = "good";
+        const std::string FEELING = "good";
 
         std::cout << "Hello world!\nHow are you?" << std::endl;
-        std::cout << "I'm " << feeling << ". How about you?" << "\n\n-----\n\n";
+        std::cout << "I'm " << FEELING << ". How about you?" << "\n\n-----\n\n";
 
         std::cout << "Give me a random integer." << std::endl;
-        std::cout << integer << "\n";
+        std::cout << INTEGER << "\n";
 
         std::cout << "How about a double?" << std::endl;
-        std::cout << adouble << std::endl;
+        std::cout << ADOUBLE << std::endl;
 
         std::cout << "A character?" << std::endl;
-        std::cout << "\"" << character << "\"" << std::endl;
+        std::cout << "\"" << CHARACTER << "\"" << std::endl;
         std::cout << "Clever." << std::endl;
 
         std::cout << "Are you alive?" << std::endl;
         if (alive)
         {
-                std::cout << "Yes" << std::endl;
-                std::cout << "Good!" << std::endl;
+                if (ORIGINAL_ALIVE_VALUE == alive)
+                {
+                        std::cout << "Yes" << std::endl;
+                        std::cout << "Good!" << std::endl;
+                }
+                else
+                {
+
+                        std::cout << "I am now." << std::endl;
+                        std::cout << "Good... Wait what?" << std::endl;
+                }
         }
         else
         {
-                std::cout << "No" << std::endl;
-                std::cout << "Huh?" << std::endl;
+                if (ORIGINAL_ALIVE_VALUE == alive)
+                {
+                        std::cout << "No" << std::endl;
+                        std::cout << "Huh?" << std::endl;
+                }
+                else
+                {
+
+                        std::cout << "I am no longer alive." << std::endl;
+                        std::cout << "Wait what? Am I talking to a corpse..?" << std::endl;
+                }
         }
 
         return 0;
