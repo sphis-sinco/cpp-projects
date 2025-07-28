ae144aff3b0cfd676f4290720d00f52a93f5ccef
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-28 17:45:13 -0500

    gonna work on variables

---
+++ projects/02 - variables/variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+        std::cout << "Hello world!" << std::endl;
+        std::cout << "How are you?" << std::endl;
+
+        std::cout << "I'm good. How about you?" << std::endl;
+
+        return 0;
+}
