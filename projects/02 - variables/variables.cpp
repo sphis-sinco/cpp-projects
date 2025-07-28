5706cfcfbfb39c6c0c00b5cfcf1e90034f2cddc9
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-28 17:47:31 -0500

    random number

--- projects/02 - variables/variables.cpp
+++ projects/02 - variables/variables.cpp
@@ -2,10 +2,15 @@
 
 int main()
 {
+        int integer = 5;
+
         std::cout << "Hello world!" << std::endl;
         std::cout << "How are you?" << std::endl;
 
         std::cout << "I'm good. How about you?" << std::endl;
+
+        std::cout << "Give me a random integer" << std::endl;
+        std::cout << integer << std::endl;
 
         return 0;
 }
