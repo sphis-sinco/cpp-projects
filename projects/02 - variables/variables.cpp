c02d592f792f424b68bdbe77005fb732b4e60576
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-28 17:49:56 -0500

    double

--- projects/02 - variables/variables.cpp
+++ projects/02 - variables/variables.cpp
@@ -2,13 +2,20 @@
 
 int main()
 {
+        // int (whole number no decimal)
         int integer = 5;
+
+        // double (number with decimal, a float)
+        double adouble = 3.14159;
 
         std::cout << "Hello world!\nHow are you?" << std::endl;
         std::cout << "I'm good. How about you?" << "\n\n";
 
         std::cout << "Give me a random integer." << std::endl;
-        std::cout << integer << std::endl;
+        std::cout << integer << "\n\n";
+
+        std::cout << "How about a double?" << std::endl;
+        std::cout << adouble;
 
         return 0;
 }
