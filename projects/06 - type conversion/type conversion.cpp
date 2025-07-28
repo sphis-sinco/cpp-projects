2e4c6dc956085084576d5851a29cfe52505effb5
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-28 18:18:26 -0500

    type conversions

--- projects/06 - type conversion/type conversion.cpp
+++ projects/06 - type conversion/type conversion.cpp
@@ -2,5 +2,20 @@
 
 int main()
 {
+        double x = (int) 3.14;
+        std::cout << x << std::endl;
+
+        // char y = 100; // d
+        char y = (char) 100; // d
+        std::cout << y << std::endl;
+
+        int correct = 8;
+        int questions = 10;
+
+        // double score = correct/questions * 100; // 0%
+        double score = correct/(double)questions * 100; // 80%
+
+        std::cout << score << "%" << std::endl;
+
         return 0;
 }
